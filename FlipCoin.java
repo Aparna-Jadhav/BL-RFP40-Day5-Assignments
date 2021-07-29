e92ab8372ab2fa6af96dd6601667af613444a7fe
Aparna-Jadhav <86564550+Aparna-Jadhav@users.noreply.github.com>
2021-07-30 00:04:52 +0530

    Add files via upload

---
+++ FlipCoin.java
@@ -0,0 +1,18 @@
+package Day5;
+
+public class FlipCoin 
+{
+	public static void main(String[] args) 
+	{
+		if (Math.random() < 0.5)
+		{
+			System.out.println("Tail");
+		}
+		else
+		{
+			System.out.println("Head");
+		}
+			
+	}
+
+}
